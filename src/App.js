7bbe8ed39e74a79230f5d24881628c3efe7c8a52
zhangyapeng1 <zhangyapeng1@tal.com>
2023-04-20 16:20:33 +0800

    fix:v3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./util"
 function App() {
   return (
     <div className="App">
+      <div className='dom'>我是dom元素</div>
      <canvas id="canvas" width="750" height="1000"></canvas>
     </div>
   );
--- src/util.js
+++ src/util.js
@@ -1,7 +1,7 @@
 import { Game, GameObject, resource, RESOURCE_TYPE,LOAD_EVENT } from '@eva/eva.js'
 import { RendererSystem } from '@eva/plugin-renderer'
 import { DragonBone, DragonBoneSystem } from '@eva/plugin-renderer-dragonbone'
-
+import { EventSystem,Event } from '@eva/plugin-renderer-event'
 resource.addResource([
   {
     name: 'dragonbone',
@@ -27,11 +27,13 @@ resource.on(LOAD_EVENT.COMPLETE, () => {
   const game = new Game({
     systems: [
       new RendererSystem({
+        // transparent: true,
         canvas: document.querySelector('#canvas'),
         width: 750,
         height: 1000
       }),
-      new DragonBoneSystem()
+      new DragonBoneSystem(),
+      new EventSystem()
     ],
       autoStart: true
   })
@@ -41,6 +43,7 @@ resource.on(LOAD_EVENT.COMPLETE, () => {
     width: 750,
     height: 1000
   }
+  // game.scene.background.transparent = true
 
   // dragonbone 的 origin 是失效的，将会按照 dragonbone 设计时的坐标重点定位
   const dragonBone = new GameObject('db', {
@@ -60,7 +63,23 @@ resource.on(LOAD_EVENT.COMPLETE, () => {
       armatureName: 'homepage_girl_ancient'
     })
   )
-  db.play('idle')
+  const evt=dragonBone.addComponent(
+    new Event({
+
+    })
+  )
+  let isComplete=true,index=0;
+  db.on('complete', () => {
+    isComplete = true
+    db.play('idle')
+  })
+  evt.on('touchstart', () => {
+    if (isComplete) {
+      isComplete = false
+      db.play(['hello', 'heart'][index++ % 2], 1)
+    }
+  })
+  db.play('idle',1)
   game.scene.addChild(dragonBone)
 })
 resource.preload()
