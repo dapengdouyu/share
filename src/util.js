be1edac7ec24b6788ba25960366ae50177d632cc
zhangyapeng1 <zhangyapeng1@tal.com>
2023-04-20 15:42:28 +0800

    fix:

--- src/util.js
+++ src/util.js
@@ -1,49 +1,67 @@
-import { Game, GameObject, resource, RESOURCE_TYPE } from '@eva/eva.js';
-import { RendererSystem } from '@eva/plugin-renderer';
-import { Img, ImgSystem } from '@eva/plugin-renderer-img';
+import { Game, GameObject, resource, RESOURCE_TYPE,LOAD_EVENT } from '@eva/eva.js'
+import { RendererSystem } from '@eva/plugin-renderer'
+import { DragonBone, DragonBoneSystem } from '@eva/plugin-renderer-dragonbone'
+
 resource.addResource([
   {
-    name: 'imageName',
-    type: RESOURCE_TYPE.IMAGE,
+    name: 'dragonbone',
+    type: RESOURCE_TYPE.DRAGONBONE,
     src: {
       image: {
         type: 'png',
-        url:'https://gw.alicdn.com/tfs/TB15Upxqk9l0K4jSZFKXXXFjpXa-750-1624.jpg',
+        url: 'https://static0.xesimg.com/tailor/game/homepage_girl_ancient/homepage_girl_ancient_tex.png'
       },
+      tex: {
+        type: 'json',
+        url: 'https://static0.xesimg.com/tailor/game/homepage_girl_ancient/homepage_girl_ancient_tex.json'
+      },
+      ske: {
+        type: 'json',
+        url: 'https://static0.xesimg.com/tailor/game/homepage_girl_ancient/homepage_girl_ancient_ske.json'
+      }
     },
-    preload: true,
-  },
-]);
+    preload: true
+  }
+])
+resource.on(LOAD_EVENT.COMPLETE, () => {
+  const game = new Game({
+    systems: [
+      new RendererSystem({
+        canvas: document.querySelector('#canvas'),
+        width: 750,
+        height: 1000
+      }),
+      new DragonBoneSystem()
+    ],
+      autoStart: true
+  })
 
-const game = new Game({
-  systems: [
-    new RendererSystem({
-      canvas: document.querySelector('#canvas'),
-      width: 750,
-      height: 1000,
-    }),
-    new ImgSystem(),
-  ],
-});
+  // 此处还在考虑如何设置默认场景的宽高
+  game.scene.transform.size = {
+    width: 750,
+    height: 1000
+  }
 
-const image = new GameObject('image', {
-  size: { width: 750, height: 1319 },
-  origin: { x: 0, y: 0 },
-  position: {
-    x: 0,
-    y: -319,
-  },
-  anchor: {
-    x: 0,
-    y: 0,
-  },
-});
+  // dragonbone 的 origin 是失效的，将会按照 dragonbone 设计时的坐标重点定位
+  const dragonBone = new GameObject('db', {
+    anchor: {
+      x: 0.5,
+      y:1
+    },
+    position: {
+      x: 0,
+      y: 3
+    },
+  })
 
-image.addComponent(
-  new Img({
-    resource: 'imageName',
-  }),
-);
+  const db=dragonBone.addComponent(
+    new DragonBone({
+      resource: 'dragonbone',
+      armatureName: 'homepage_girl_ancient'
+    })
+  )
+  db.play('idle')
+  game.scene.addChild(dragonBone)
+})
+resource.preload()
 
-game.scene.addChild(image);
-game.start()
